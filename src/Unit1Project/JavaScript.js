85dec2c16b35b66b71a1de5835b7b7803ad84b67
Brian <bleaskey@msn.com>
2017-03-23 08:17:14 -0500

    Final commit

--- src/Unit1Project/JavaScript.js
+++ src/Unit1Project/JavaScript.js
@@ -1,4 +1,35 @@
-﻿function altClass()
-{
+﻿function alt1() {
+    label1.style.textDecoration="line-through";
+}
 
+function alt2() {
+    label2.style.textDecoration = "line-through";
+}
+
+function alt3() {
+    label3.style.textDecoration = "line-through";
+}
+
+function alt4() {
+    label4.style.textDecoration = "line-through";
+}
+
+function alt5() {
+    label5.style.textDecoration = "line-through";
+}
+
+function alt6() {
+    label6.style.textDecoration = "line-through";
+}
+
+function alt7() {
+    label7.style.textDecoration = "line-through";
+}
+
+function alt8() {
+    label8.style.textDecoration = "line-through";
+}
+
+function alt9() {
+    label9.style.textDecoration = "line-through";
 }
