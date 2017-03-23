7505d66a26c9c1448cbd82ff603971947f585990
Brian <bleaskey@msn.com>
2017-03-23 11:04:25 -0500

    Finished Unit 1 Project

--- src/Unit1Project/JavaScript.js
+++ src/Unit1Project/JavaScript.js
@@ -1,35 +1,90 @@
-﻿function alt1() {
-    label1.style.textDecoration="line-through";
+﻿function alt1(label1) {
+    var className = label1.getAttribute("class");
+    if (className == "l1") {
+        label1.className = "alt1";
+    }
+    else {
+        label1.className = "l1";
+    }
 }
 
-function alt2() {
-    label2.style.textDecoration = "line-through";
+function alt2(label2) {
+    var className = label2.getAttribute("class");
+    if (className == "l2") {
+        label2.className = "alt2";
+    }
+    else {
+        label2.className = "l2";
+    }
 }
 
-function alt3() {
-    label3.style.textDecoration = "line-through";
+function alt3(label3) {
+    var className = label3.getAttribute("class");
+    if (className == "l3") {
+        label3.className = "alt3";
+    }
+    else {
+        label3.className = "l3";
+    }
 }
 
-function alt4() {
-    label4.style.textDecoration = "line-through";
+function alt4(label4) {
+    var className = label4.getAttribute("class");
+    if (className == "l4") {
+        label4.className = "alt4";
+    }
+    else {
+        label4.className = "l4";
+    }
 }
 
-function alt5() {
-    label5.style.textDecoration = "line-through";
+function alt5(label5) {
+    var className = label5.getAttribute("class");
+    if (className == "l5") {
+        label5.className = "alt5";
+    }
+    else {
+        label5.className = "l5";
+    }
 }
 
-function alt6() {
-    label6.style.textDecoration = "line-through";
+function alt6(label6) {
+    var className = label6.getAttribute("class");
+    if (className == "l6") {
+        label6.className = "alt6";
+    }
+    else {
+        label6.className = "l6";
+    }
 }
 
-function alt7() {
-    label7.style.textDecoration = "line-through";
+function alt7(label7) {
+    var className = label7.getAttribute("class");
+    if (className == "l7") {
+        label7.className = "alt7";
+    }
+    else {
+        label7.className = "l7";
+    }
 }
 
-function alt8() {
-    label8.style.textDecoration = "line-through";
+function alt8(label8) {
+    var className = label8.getAttribute("class");
+    if (className == "l8") {
+        label8.className = "alt8";
+    }
+    else {
+        label8.className = "l8";
+    }
 }
 
-function alt9() {
-    label9.style.textDecoration = "line-through";
+function alt9(label9) {
+    var className = label9.getAttribute("class");
+    if (className == "l9") {
+        label9.className = "alt9";
+    }
+    else {
+        label9.className = "l9";
+    }
 }
+
